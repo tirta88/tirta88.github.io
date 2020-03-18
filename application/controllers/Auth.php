282b8f8cac97f55f7dd6e114fb21a7ae8cc26409
tirtaiputu <tirtaiputu88@gmail.com>
2020-03-18 11:17:59 +0800

    initial commit

---
+++ application/controllers/Auth.php
@@ -0,0 +1,73 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Auth extends CI_Controller
+{
+
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->library('form_validation');
+	}
+	public function index()
+	{
+		$this->form_validation->set_rules('email', 'Email', 'trim|required|valid_email');
+		$this->form_validation->set_rules('password', 'Password', 'trim|required');
+
+		if ($this->form_validation->run() == false) {
+			$data['title'] = 'Login Page';
+			$this->load->view('templates/auth_header', $data);
+			$this->load->view('auth/login');
+			$this->load->view('templates/auth_footer');
+		} else {
+
+			// validasinya success
+			$this->_login();
+		}
+	}
+
+
+
+	private function _login()
+	{
+		$email = $this->input->post('email');
+		$password = $this->input->post('password');
+
+		$user = $this->db->get_where('user', ['email' => $email])->row_array();
+		var_dump($user);
+		die;
+	}
+
+	public function registration()
+	{
+		$this->form_validation->set_rules('name', 'Name', 'required|trim');
+		$this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email|is_unique[user.email]', [
+			'is_unique' => 'This email has already registered!'
+		]);
+		$this->form_validation->set_rules('password1', 'Password', 'required|trim|min_length[3]|matches[password2]', ['matches' => 'password dont match!', 'min_length' => 'Password too short!']);
+		$this->form_validation->set_rules('password2', 'Password', 'required|trim|matches[password1]');
+
+		if ($this->form_validation->run() == false) {
+			$data['title'] = 'WPU User Registration';
+			$this->load->view('templates/auth_header', $data);
+			$this->load->view('auth/registration');
+			$this->load->view('templates/auth_footer');
+		} else {
+			$data = [
+				'name' => htmlspecialchars($this->input->post('name')),
+				'email' => htmlspecialchars($this->input->post('email')),
+				'image' => 'default.jpg',
+				'password' => password_hash($this->input->post('password'), PASSWORD_DEFAULT),
+				'role_id' => 2,
+				'is_active' => 1,
+				'date_created' => time()
+
+			];
+
+			$this->db->insert('user', $data);
+			$this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Congratulation! your account has been created. Please Login</div>');
+
+			redirect('auth');
+		}
+	}
+}
